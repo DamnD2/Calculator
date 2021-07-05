8632e43083c3eb7085cf846f9429770cf0f9f43b
Vitalii <vitalya_daineko@mail.ru>
2021-07-05 11:47:14 +0300

    code revision

--- js/main.js
+++ js/main.js
@@ -7,33 +7,35 @@ document.querySelector('.calculator').addEventListener("click", event => {
 });
 
 function calc ( operator, firstOperand, secondOperand ) {
-	const _firstOperand = parseFloat(firstOperand);
-	const _secondOperand = parseFloat(secondOperand);
+	const convertedFirstOperand = parseFloat(firstOperand);
+	const convertedSecondOperand = parseFloat(secondOperand);
 	let result = 0;
+
 	switch(operator){
 		case "+":{
-			result = _firstOperand + _secondOperand ;
+			result = convertedFirstOperand + convertedSecondOperand ;
 			break;
 		} 
 		case "-": {
-			result = _firstOperand - _secondOperand;
+			result = convertedFirstOperand - convertedSecondOperand;
 			break;
 		}
 		case "/": {
-			result = _firstOperand / _secondOperand;
+			result = convertedFirstOperand / convertedSecondOperand;
 			break;
 		}
 		case "*": {
-			result = _firstOperand * _secondOperand;
+			result = convertedFirstOperand * convertedSecondOperand;
 			break;
 		}
 	}
+
 	if(result <= Math.pow(10, (-maxResultFieldLength)) && result > 0){
 		return result.toExponential(4);
 	} else if (result >= Math.pow(10, maxResultFieldLength) || result <= -Math.pow(10, maxResultFieldLength - 1)) {
 		return result.toExponential(4);
 	} else if (result.toString().length > maxResultFieldLength){
-		return result.toFixed(maxResultFieldLength);
+		return result.toFixed(3);
 	} else {
 		return result;
 	}
@@ -41,64 +43,66 @@ function calc ( operator, firstOperand, secondOperand ) {
 
 
 function showChanges( target ){
-	const $resultField = document.querySelector('.result');
-	const result = $resultField.innerText;
-	const $expressionField = document.querySelector('.expression');
-	const expression = $expressionField.innerText;
-	const firstOperand = expression.split(" ")[0];
-	const operator = expression.split(" ")[1];
+	const resultField = document.querySelector('.result');
+	const resultFieldValue = resultField.innerText;
+	const expressionField = document.querySelector('.expression');
+	const expression = expressionField.innerText;
+	const firstOperandIndex = 0;
+	const operatorIndex = 1;
+	const firstOperand = expression.split(" ")[firstOperandIndex];
+	const operator = expression.split(" ")[operatorIndex];
 	const mathOperator = getMathOperator(operator);
 
 
 	if (target.classList.contains('numeral')){
-		if (target.innerText === "." && result.indexOf(".") !== -1){ 
+		if (target.innerText === "." && resultFieldValue.indexOf(".") !== -1){ 
 			return;
 		}
 		if (lastClickedItem && lastClickedItem.classList.contains('operator')){
-			$resultField.innerText = target.innerText;
+			resultField.innerText = target.innerText;
 		}
-		if ($resultField.innerText.length <= maxResultFieldLength){
-			if ($resultField.innerText === "0" && target.innerText !== "."){
-				$resultField.innerText = target.innerText;
+		if (resultField.innerText.length <= maxResultFieldLength){
+			if (resultField.innerText === "0" && target.innerText !== "."){
+				resultField.innerText = target.innerText;
 			} else {
-				$resultField.innerText += target.innerText;
+				resultField.innerText += target.innerText;
 			}
 		}
-		if (operator && result === firstOperand && lastClickedItem.classList.contains('operator')){
-			$resultField.innerText = target.innerText;
+		if (operator && resultFieldValue === firstOperand && lastClickedItem.classList.contains('operator')){
+			resultField.innerText = target.innerText;
 		}
 		if (expression.indexOf("=") !== -1){
-			$expressionField.innerText = "";
-			$resultField.innerText = target.innerText;
+			expressionField.innerText = "";
+			resultField.innerText = target.innerText;
 		}
 	}
 
 
 	if (target.classList.contains("operator")){
 		if ( !operator || operator !== target.innerText){
-			$expressionField.innerText = `${result} ${target.innerText} `;
+			expressionField.innerText = `${resultFieldValue} ${target.innerText} `;
 		}
 		if (expression.indexOf("=") !== -1 ) {
-			$expressionField.innerText = `${result} ${target.innerText}`;
+			expressionField.innerText = `${resultFieldValue} ${target.innerText}`;
 		} else if ( operator && lastClickedItem.classList.contains('numeral') ){
-			$resultField.innerText = calc( mathOperator, firstOperand, result );
-			$expressionField.innerText = `${calc( mathOperator, firstOperand, result )} ${target.innerText} `;
+			resultField.innerText = calc( mathOperator, firstOperand, resultFieldValue );
+			expressionField.innerText = `${calc( mathOperator, firstOperand, resultFieldValue )} ${target.innerText} `;
 		}
 	}
 
 
 	if (target.classList.contains("calculator__clear")){
-		$expressionField.innerText = "";
-		$resultField.innerText = "0";
+		expressionField.innerText = "";
+		resultField.innerText = "0";
 	}
 
 
 	if (target.classList.contains("equals")){
 		if (!expression || operator === "="){
-			$expressionField.innerText = `${result} = `;
+			expressionField.innerText = `${resultFieldValue} = `;
 		} else {
-			$resultField.innerText = calc( mathOperator, firstOperand, result);
-			$expressionField.innerText = `${firstOperand} ${operator} ${result} = `;
+			resultField.innerText = calc( mathOperator, firstOperand, resultFieldValue);
+			expressionField.innerText = `${firstOperand} ${operator} ${resultFieldValue} = `;
 		}
 	}
 }
